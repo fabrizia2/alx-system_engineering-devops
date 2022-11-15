6fb384036c43bd03fe825dc98ac010cacfb9ce30
fabrizia2 <melvinrenish56@gmail.com>
2022-11-15 12:08:30 -0800

    The flags that were used

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,4 +3,4 @@
 # The receiver phone number or name (including country code if present)
 # The flags that were used
 
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")
