8216593e5e0ebe78938bfd7b642f7ae9608c39cf
fabrizia2 <melvinrenish56@gmail.com>
2023-02-28 21:59:30 +0300

    2-recurse.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
-"""count_dict argument to keep track of the word counts"""
+""" recursive function that queries the Reddit API"""
+import requests
+
+#!/usr/bin/python3
+""" recursive function that queries the Reddit API"""
+import requests
+import sys
+after = None
+count_dic = []
 
 
-def count_words(subreddit, word_list, count_dict=None):
-    """count_dict argument to keep track of the word counts"""
-    import requests
-    import re
-
-    if count_dict is None:
-        count_dict = {}
-
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        return None
-
-    data = response.json()['data']['children']
-
-    for post in data:
-        title = post['data']['title'].lower()
-        for word in word_list:
-            if word.lower() in re.findall(r'\b[a-z]+\b', title):
-                if word.lower() in count_dict:
-                    count_dict[word.lower()] += 1
-                else:
-                    count_dict[word.lower()] = 1
-
-    if 'after' in response.json()['data']:
-        after = response.json()['data']['after']
-        count_dict = count_words(subreddit, word_list, count_dict=count_dict)
-
-    sorted_count = sorted(count_dict.items(), key=lambda x: (-x[1], x[0]))
-
-    for word, count in sorted_count:
-        print(word + ' ' + str(count))
-
-    return count_dict
+def count_words(subreddit, word_list):
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces) """
+    global after
+    global count_dic
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
-""" function takes subreddit name and hot_list argument as input."""
+""" 100-count
+    Function to count words in all hot posts of a given Reddit subreddit."""
+import requests
+
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """function takes a subreddit name and hot_list argument as input."""
+    """
+        Returns the number of hot posts for a given subreddit.
+        Returns 0 if invalid subreddit was given
+    """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
+    headers = {
+            "User-Agent": "redditdev scraper by u/coderboy-exe",
+            "From": "coderboy.exe@gmail.com"
+    }
+    params = {
+            "limit": 100
+    }
+
+    if after:
+        params["after"] = after
+
+    res = requests.get(url, headers=headers, params=params)
+    if res.status_code == 200:
+        data = res.json()
 
         hot_posts = data.get("data", {}).get("children", [])
         for post in hot_posts:
