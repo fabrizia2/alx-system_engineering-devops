28f67c9f930ecb0ff3c241843bdba1e80e528670
fabrizia2 <melvinrenish56@gmail.com>
2023-02-28 18:10:19 +0300

    How

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     """Returns number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False).json()
+    response = requests.get(url, headers=headers).json()
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
